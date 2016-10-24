984363cea33b4c6591387e9cc3d20e689fa44555
Joshua Cox <joshua.n.cox@gmail.com>
2016-10-24 13:48:08 -0700

    update:templates removed superfluous headers

--- runserver.py
+++ runserver.py
@@ -28,6 +28,6 @@ if __name__ == '__main__':
             print('Database tables created')
 
     else:
-        host = os.environ.get('HOST', 'localhost')
+        host = os.environ.get('HOST', '0.0.0.0')
         port = int(os.environ.get('PORT', 5000))
         app.run(host=host, port=port)
